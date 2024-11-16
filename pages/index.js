b6c2ec6511c44a166d340c95867fb1c676955745
jaimealonso7 <126238584+jaimealonso7@users.noreply.github.com>
2024-11-16 17:21:40 +0100

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class MainView extends React.Component {
+    render() {
+        return(
+            <h1>Hello from NextJS!</h1>
+        )
+    }
+}
+
+export default MainView
